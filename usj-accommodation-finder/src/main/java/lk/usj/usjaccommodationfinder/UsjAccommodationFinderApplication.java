16013290672c3c3cb04777d8f73684e403535aee
hashan <hashan.maduwantha@madmobile.com>
2023-04-01 16:15:59 +0530

    initial commit - project template created

---
+++ usj-accommodation-finder/src/main/java/lk/usj/usjaccommodationfinder/UsjAccommodationFinderApplication.java
@@ -0,0 +1,13 @@
+package lk.usj.usjaccommodationfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsjAccommodationFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsjAccommodationFinderApplication.class, args);
+	}
+
+}
---
+++ usj-accommodation-finder/src/test/java/lk/usj/usjaccommodationfinder/UsjAccommodationFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.usj.usjaccommodationfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsjAccommodationFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
